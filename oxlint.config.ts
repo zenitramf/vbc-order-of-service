4d44b538be9ed2f8c8513c57274fa7c8e17f08c5
zenitramf <francisco@zenitram.dev>
2026-06-21 01:45:49 +0000

    Ultracite update

---
+++ oxlint.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from "oxlint";
+import core from "ultracite/oxlint/core";
+import react from "ultracite/oxlint/react";
+import tanstack from "ultracite/oxlint/tanstack";
+
+export default defineConfig({
+  extends: [core, react, tanstack],
+  ignorePatterns: core.ignorePatterns,
+});
