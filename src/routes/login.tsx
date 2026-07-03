a1a3d2fd719fc7df6294399a8e5592a212e19ad4
zenitramf <francisco@zenitram.dev>
2026-07-03 19:06:37 +0000

    login remove second auth

--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -20,15 +20,6 @@ const HOME_PATH = "/";
 const TEMPORARY_LOGIN_IMAGE_URL =
   "https://upload.wikimedia.org/wikipedia/commons/b/bf/Downtown_Fresno_Skyline_2021.jpg";
 
-const GitHubIcon = () => (
-  <svg aria-hidden="true" viewBox="0 0 24 24">
-    <path
-      d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"
-      fill="currentColor"
-    />
-  </svg>
-);
-
 type LoginFormProps = ComponentProps<"form"> & {
   isSubmitting: boolean;
 };
@@ -46,10 +37,9 @@ const LoginForm = ({ className, isSubmitting, ...props }: LoginFormProps) => (
         <FieldLabel htmlFor="email">Email</FieldLabel>
         <Input
           autoCapitalize="none"
-          autoComplete="email"
           id="email"
           name="email"
-          placeholder="m@example.com"
+          placeholder="me@example.com"
           required
           type="email"
         />
@@ -57,15 +47,14 @@ const LoginForm = ({ className, isSubmitting, ...props }: LoginFormProps) => (
       <Field>
         <div className="flex items-center">
           <FieldLabel htmlFor="password">Password</FieldLabel>
-          <button
+          {/*<button
             className="ml-auto text-sm underline-offset-4 hover:underline"
             type="button"
           >
             Forgot your password?
-          </button>
+          </button>*/}
         </div>
         <Input
-          autoComplete="current-password"
           id="password"
           minLength={4}
           name="password"
@@ -78,10 +67,9 @@ const LoginForm = ({ className, isSubmitting, ...props }: LoginFormProps) => (
           {isSubmitting ? <Spinner /> : "Login"}
         </Button>
       </Field>
-      <FieldSeparator>Or continue with</FieldSeparator>
-      <Field>
+      {/*<FieldSeparator>Or continue with</FieldSeparator>*/}
+      {/*<Field>
         <Button disabled type="button" variant="outline">
-          <GitHubIcon />
           Login with GitHub
         </Button>
         <FieldDescription className="text-center">
@@ -90,7 +78,7 @@ const LoginForm = ({ className, isSubmitting, ...props }: LoginFormProps) => (
             Sign up
           </button>
         </FieldDescription>
-      </Field>
+      </Field>*/}
     </FieldGroup>
   </form>
 );
@@ -147,4 +135,7 @@ export const LoginPage = () => {
 
 export const Route = createFileRoute("/login")({
   component: LoginPage,
+  loader: async () => {
+    await import("~/lib/auth-client");
+  },
 });
